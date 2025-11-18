d655355604163e8bb8128b2454ff4a82eaaae2f4
John Kim <johnkim@Johns-MacBook-Air-5.local>
2025-11-18 09:39:28 -0500

    common dataset folder

--- cnn_classification.py
+++ cnn_classification.py
@@ -183,8 +183,8 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,)),
     ])
-    train_dataset = datasets.FashionMNIST('./data', train=True, download=True, transform=transform)
-    test_dataset = datasets.FashionMNIST('./data', train=False, download=True, transform=transform)
+    train_dataset = datasets.FashionMNIST('./datasets', train=True, download=True, transform=transform)
+    test_dataset = datasets.FashionMNIST('./datasets', train=False, download=True, transform=transform)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
